e151571ab0bc249a85d692a7972c3df45ff30b5f
Deividas Verbickas <deivverb@gmail.com>
2023-10-30 19:58:52 +0200

    setup shortcut descriptions for which-key

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -1,12 +1,15 @@
 local builtin = require('telescope.builtin')
+
 -- in Normal mode search for Project Files
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>pf', builtin.find_files, { desc = 'Search Project' })
+
 -- in Normal mode search for git files
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, { desc = 'Search Git Project' })
+
 -- in Normal mode search for specific string (requires ripgrep) 
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input('Grep > ') });
-end)
+end, { desc = 'Search Project (Grep)' })
 
 require('telescope').setup {
     defaults = {
